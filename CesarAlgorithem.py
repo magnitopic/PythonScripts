69c3b51e1a97e1f092ee7d3a41c1e0717524cfaa
magnitopic <magnitopic@gmail.com>
2022-01-19 21:54:58 +0100

    CesarAlgorithem start

---
+++ CesarAlgorithem.py
@@ -0,0 +1,10 @@
+def encript():
+	pass
+def decript():
+	pass
+if __name__ == "__main__":
+	option=input("Do you want to encript or decript?: ").lower
+	if option=="encript":
+		encript()
+	elif option=="decript":
+		decript()
