802efe57d07ff1898006597686f67011bc318253
magnitopic <magnitopic@gmail.com>
2022-01-27 11:51:25 +0100

    adde rotations

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,31 +1,35 @@
-leters = list("abcdefghijklmnopkrstuvwxyz")
+from itertools import cycle
 
 
-def encript(text):
+leters = list("abcdefghijklmnñopkrstuvwxyz")
+
+
+def encript(text, rotations):
     text = list(text)
     for i in range(len(text)):
         for j in range(len(leters)):
             if text[i] == leters[j]:
-                text[i] = leters[j-23]
+                text[i] = leters[j-len(leters)+rotations]
                 break
     return ''.join(text)
 
 
-def decript(text):
+def decript(text, rotations):
     text = list(text)
     for i in range(len(text)):
         for j in range(len(leters)):
             if text[i] == leters[j]:
-                text[i] = leters[j-3]
+                text[i] = leters[j-rotations]
                 break
     return ''.join(text)
 
 
 if __name__ == "__main__":
     option = input("Do you want to encript or decript?: ").lower()
+    rotations = int(input("How may rotations?: "))
     if option == "encript":
         text = input("What text do you want to encript?: ").lower()
-        print(encript(text))
+        print(encript(text, rotations))
     elif option == "decript":
         text = input("What text do you want to decript?: ").lower()
-        print(decript(text))
+        print(decript(text, rotations))
