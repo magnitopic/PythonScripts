e57c424c805750138ac4b6799a6ede03d72188b5
magnitopic <magnitopic@gmail.com>
2022-05-15 21:07:54 +0200

    pi book

---
+++ randomPiBook.py
@@ -0,0 +1,34 @@
+""" This program writes a book with random words that have the same
+	length as the digit of Pi(π) that's in that position. Example:
+	First word would have 3 letters, second 1, third 4... and so on.
+"""
+import requests, json, random, numpy as np
+from mpmath import mp
+
+
+def newPiBook(nWords=100):
+    book = ""
+    # Source for all words in the English laguage:
+    # https://github.com/dwyl/english-words
+    request = requests.get('https://raw.githubusercontent.com/dwyl/english-words/master/words.txt')
+    # parse data to make a list of words
+    words = str(request.text).strip().split("\n")
+
+    # pi characters we need
+    mp.dps = nWords
+    # list of pi numbers
+    piNum = list(str(mp.pi).replace(".", ""))
+
+    for i in piNum:
+        # Get all words that have the correct length
+        wordList = ["."]
+        for word in words:
+            if len(word) == int(i):
+                wordList.append(word.lower())
+        # Pic a random word from the list
+        book += random.choice(wordList)+" "
+    return book.capitalize()
+
+
+if __name__ == "__main__":
+    print(newPiBook())
