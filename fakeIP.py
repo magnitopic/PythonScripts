25630ca3b2b8c729309cc6c9ce20567995f7286b
magnitopic <magnitopic@gmail.com>
2022-06-23 22:42:47 +0200

    Updated to make a valid ip

--- fakeIP.py
+++ fakeIP.py
@@ -1,11 +1,11 @@
 import random
 
-print("You are now going to be hacked, do you consent? (Y,N)")
+input("You are now going to be hacked, do you consent? (Y,N)")
 print("HAHAHHAHA, nevermind your consent was never neccessary")
 print("Your IP is as follows:")
 
 fakeip = ""
 for x in range(4):
-    fakeip += str(random.randint(0, 999))+"."
+    fakeip += str(random.randint(0, 256))+"."
 
 print(fakeip[:-1])
