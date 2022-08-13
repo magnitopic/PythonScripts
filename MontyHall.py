8b98ee20bf32a6d02a87926b1d59c2a10ff4aae9
magnitopic <magnitopic@gmail.com>
2022-08-13 10:01:21 +0200

    on the half way point

--- MontyHall.py
+++ MontyHall.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 # root window
 root = Tk()
@@ -7,14 +8,26 @@ h = int(root.winfo_screenheight()/3)
 root.geometry(f"{w}x{h}")
 root.resizable(False, False)
 root.title('Button Demo')
+# vars
+doors = [0]*3
+doors[random.randrange(3)] = 1
+roundNum = 0
 
-def selection():
-	
 
-# exit button
+def selection(i, button):
+	""" if roundNum % 2:
+
+		roundNum += 1
+	if doors[i]: """
+	button.config(text="Presed")
+
+
+# Buttons and positioning
 Button(root, text='Quit', command=lambda: root.quit()).place(x=w*50/100, y=h/1.5)
-Button(root, text="Left", command=selection("left")).place(x=w*25/100, y=h/2)
-Button(root, text="Center").place(x=w*50/100, y=h/2)
-Button(root, text="Right").place(x=w*75/100, y=h/2)
+one = Button(root, text="Left", command=lambda: selection(0, one)).place(x=w*25/100, y=h/2)
+two = Button(root, text="Center", command=lambda: selection(1, two)).place(x=w*50/100, y=h/2)
+tree = Button(root, text="Right", command=lambda: selection(2, tree)).place(x=w*75/100, y=h/2)
+Label(root, text=doors).place(x=w*75/100, y=h/1.6)
 
-root.mainloop()
+if __name__ == "__main__":
+    root.mainloop()
